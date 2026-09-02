d4b9dcc6ee556867388a8267e2d2d694719b101f
miyaraadi <miyara.adi@gmail.com>
2026-09-02 20:29:25 +0300

    Create route.js

---
+++ app/api/mission/[id]/route.js
@@ -0,0 +1,25 @@
+import missions from "../../../../../missions.json";
+
+export async function GET(request, context) {
+  const { id } = await context.params;
+
+  const missionTitle = missions[id];
+
+  if (!missionTitle) {
+    return Response.json(
+      {
+        success: false,
+        message: "Mission not found",
+      },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  return Response.json({
+    success: true,
+    mission_id: id,
+    mission_title: missionTitle,
+  });
+}
